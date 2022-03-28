1a26af97ef1a672e7548856a8bf7f293b2a1ff87
Jawaher Alqotym <63616896+jawaher-alqotym@users.noreply.github.com>
2022-03-28 03:11:06 +0300

    Add files via upload

---
+++ Coderhub-challenge-2.py
@@ -0,0 +1,13 @@
+# Description of the challenge
+# Write a function that receives an array of type integer
+# , checks if a given integer is in the ** array or not**
+# , and returns the result of boolean type.
+
+def find_element(array, element):
+ return element in array
+
+
+
+
+ar = [2, 4, 1 ,9, 42]
+print(find_element(ar,4))
