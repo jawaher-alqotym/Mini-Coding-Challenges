ff9a2fb34f4a4373ca4034d125de940b8b0cd038
Jawaher Alqotym <63616896+jawaher-alqotym@users.noreply.github.com>
2022-03-30 06:32:51 +0300

    Add files via upload

---
+++ Coderhub-challenge-5.py
@@ -0,0 +1,26 @@
+
+#Write a function that receives an array of type integer,
+#and the function returns the prime numbers in that array.
+
+def is_prime(n):
+    # any number less thane or = 1 is not prime
+    if (n <= 1):
+        return False
+
+    # Check from 2 to n
+    for i in range(2, int(n)):
+        if (n % i == 0):
+            return False
+
+    return True
+
+def primes_nums(array):
+    array2 = []
+    for i in range(len(array)):
+        if (is_prime(array[i])):
+            array2.append(array[i])
+
+    return array2
+
+s = [33, 4, 44, 89, 33, 7]
+print(primes_nums(s))
