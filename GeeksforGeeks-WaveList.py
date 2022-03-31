b0bb79c81faf8bb531f4aa07b68bbebfd8a2bfbc
Jawaher Alqotym <63616896+jawaher-alqotym@users.noreply.github.com>
2022-03-31 04:37:10 +0300

    Add files via upload

---
+++ GeeksforGeeks-WaveList.py
@@ -0,0 +1,17 @@
+
+# Function to sort the array into a wave-like array.
+# input -> [1,2,3,4,5]   output -> 2 1 4 3 5
+def convertToWave(arr):
+
+    for i in range(0, len(arr), 2):
+        if( i != len(arr)-1):
+            temp = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = temp
+
+        else:
+            return arr
+    return arr
+
+s = [2,4,7,8,9,10]
+print(convertToWave(s))
