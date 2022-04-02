035fe1f28ac784682e5dddec85bbb2ba3fe164d1
Jawaher Alqotym <63616896+jawaher-alqotym@users.noreply.github.com>
2022-04-02 09:04:48 +0300

    Update GeeksforGeeks-ReverseString.py

--- GeeksforGeeks-ReverseString.py
+++ GeeksforGeeks-ReverseString.py
@@ -2,12 +2,10 @@
 #You are given a string s. You need to reverse the string.
 
 def reverseWord(s):#Geeks
-    str3 = ''
     str2 = " ".join(s)#G e e k s
     list1 = str2.rsplit(" ")#['G', 'e', 'e', 'k', 's']
     list1.reverse()#['S', 'e', 'e', 'G']
-    for i in list1:#reverse the list back to string
-        str3 += i
+    str3 = ''.join(list1)
 
     return str3
 
