cf287aa3aa5600f6b7ae9eb701facf5534894f79
Jawaher Alqotym <63616896+jawaher-alqotym@users.noreply.github.com>
2022-04-27 07:32:44 +0300

    Update GeeksforGeeks-Subarray_WithGiven_Sum.py

--- GeeksforGeeks-Subarray_WithGiven_Sum.py
+++ GeeksforGeeks-Subarray_WithGiven_Sum.py
@@ -3,12 +3,6 @@
 # find a continuous sub-array which adds to a given number S.
 # In case of multiple subarrays, return the subarray which comes first
 # on moving from left to right.
-def finde_indexes(arr,list1):
-    list_indexses = [arr.index(i) for i in list1 ]
-    a = list_indexses[0]
-    b = list_indexses[len(list_indexses)-1]
-    return str(a)+' to '+str(b)
-
 def subArraySum(arr, n, s):
     j = 0
     sum = 0
@@ -26,8 +20,8 @@ def subArraySum(arr, n, s):
         j+=1
      else:
         break
-    print(arr,' indexes from', finde_indexes(arr,list1),'which contain the values:',list1, " Sums to ", s)
+    print(arr,' contain the subset:',list1, " that Sums to ", s)
 
 list = [1,2,3,7,2]
-s = 12
+s = 14
 subArraySum(list,len(list),s)
