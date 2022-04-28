f662d9c3ea1e8d8011beface9042e2632bf2c314
Jawaher Alqotym <63616896+jawaher-alqotym@users.noreply.github.com>
2022-04-28 03:26:03 +0300

    Update GeeksforGeeks-Subarray_WithGiven_Sum.py

--- GeeksforGeeks-Subarray_WithGiven_Sum.py
+++ GeeksforGeeks-Subarray_WithGiven_Sum.py
@@ -6,22 +6,30 @@
 def subArraySum(arr, n, s):
     j = 0
     sum = 0
-    list1=[]
-    while j<n:
-     for i in range(j,n):
-        list1.append(arr[i])
-        sum += arr[i]
-        if sum == s:
+    list1 = []
+
+    while j < n:
+        for i in range(j, n):
+            list1.append(i)
+            sum += arr[i]
+            if sum == s:
+                break
+
+        if sum != s:
+            list1.clear()
+            sum = 0
+            j += 1
+        else:
             break
 
-     if sum != s:
-        list1.clear()
-        sum=0
-        j+=1
-     else:
-        break
-    print(arr,' contain the subset:',list1, " that Sums to ", s)
+    x = len(list1)
 
-list = [1,2,3,7,2]
-s = 14
-subArraySum(list,len(list),s)
+    if x == 0:
+        return [-1]
+    else:
+        return 'Values in indexes ',[list1[0] + 1, list1[x - 1] + 1],'results in',s
+    
+list = [107, 41, 143, 65, 49]
+s = 184
+print(subArraySum(list,len(list),s))
+
