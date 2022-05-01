1f83bc2f48b4e80719b881bd9a4a85b352fb277f
Jawaher Alqotym <63616896+jawaher-alqotym@users.noreply.github.com>
2022-05-01 05:42:16 +0300

    Update CoderHub-Binary_toOctal_asInt.py

--- CoderHub-Binary_toOctal_asInt.py
+++ CoderHub-Binary_toOctal_asInt.py
@@ -5,11 +5,7 @@ def bin_to_oct(b):
    num_bin = int(b,2)
    num_oct = oct(num_bin)
    str1 = str(num_oct)#to remove the 0o from 234: 0o234 -> 234
-   list1 = [i for i in str1]
-   list1.pop(0)
-   list1.pop(0)
-   str2 = ''.join(list1)
-
+   str2 = str1[2:len(str1):1]
    return int(str2)
 
 
