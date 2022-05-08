1ac863556eb165af61d0edbee56f5230e79b298e
Jawaher Alqotym <63616896+jawaher-alqotym@users.noreply.github.com>
2022-05-08 04:53:58 +0300

    Update CodeWars-SeparatingStrings.py

--- CodeWars-SeparatingStrings.py
+++ CodeWars-SeparatingStrings.py
@@ -1,14 +1,23 @@
 
 # Create a function that takes a string and separates it into a sequence of letters.
-# Input:"Just Live Life Man"
 # The function should separate each word into individual letters,
 # with the first word in the sentence having its letters in the 0th index 
 # of each 2nd dimension array, and so on.
+#----
+# Input:"The Mitochondria is the powerhouse of the cell"
 # The array will be formatted as so:
-# [['J','L','L','M']
-# ,['u','i','i','a']
-# ,['s','v','f','n']
-# ,['t','e','e','']]
+# => [ [ 'T', 'M', 'i', 't', 'p', 'o', 't', 'c' ],
+# => [ 'h', 'i', 's', 'h', 'o', 'f', 'h', 'e' ],
+# => [ 'e', 't', '', 'e', 'w', '', 'e', 'l' ],
+# => [ '', 'o', '', '', 'e', '', '', 'l' ],
+# => [ '', 'c', '', '', 'r', '', '', '' ],
+# => [ '', 'h', '', '', 'h', '', '', '' ],
+# => [ '', 'o', '', '', 'o', '', '', '' ],
+# => [ '', 'n', '', '', 'u', '', '', '' ],
+# => [ '', 'd', '', '', 's', '', '', '' ],
+# => [ '', 'r', '', '', 'e', '', '', '' ],
+# => [ '', 'i', '', '', '', '', '', '' ],
+# => [ '', 'a', '', '', '', '', '', '' ]]
 
 def sep_str(st):
     bf_list = [(' '.join(i)).rsplit(' ') for i in st.split()]
