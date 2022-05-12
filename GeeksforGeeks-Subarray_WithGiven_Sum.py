3e954860cd449ed00c78357cf029ad4a55771e58
Jawaher Alqotym <63616896+jawaher-alqotym@users.noreply.github.com>
2022-05-12 15:05:45 +0300

    Update GeeksforGeeks-Subarray_WithGiven_Sum.py

--- GeeksforGeeks-Subarray_WithGiven_Sum.py
+++ GeeksforGeeks-Subarray_WithGiven_Sum.py
@@ -4,32 +4,16 @@
 # In case of multiple subarrays, return the subarray which comes first
 # on moving from left to right.
 def subArraySum(arr, n, s):
-    j = 0
-    sum = 0
-    list1 = []
+    res = []
+    while arr:
+        num = arr.pop()
+        diff = s - num
+        if diff in arr:
+            res.append((diff, num))
 
-    while j < n:
-        for i in range(j, n):
-            list1.append(i)
-            sum += arr[i]
-            if sum == s:
-                break
+    return res
 
-        if sum != s:
-            list1.clear()
-            sum = 0
-            j += 1
-        else:
-            break
-
-    x = len(list1)
-
-    if x == 0:
-        return [-1]
-    else:
-        return 'Values in indexes ',[list1[0] + 1, list1[x - 1] + 1],'results in',s
-    
-list = [107, 41, 143, 65, 49]
-s = 184
+list = [1,2,5,33,11,23,12,90,87,4]
+s = 12
 print(subArraySum(list,len(list),s))
 
